a97f626846edfce1bab576f703eb640489aeb445
xiaojian <307041936@qq.com>
2018-12-03 11:07:50 +0800

    删除支付完成回调/取消支付回调/退款回调地址字段

--- base-data/src/main/java/com/lhiot/dc/entity/PaymentConfig.java
+++ base-data/src/main/java/com/lhiot/dc/entity/PaymentConfig.java
@@ -52,24 +52,6 @@ public class PaymentConfig {
     private String thirdPartyKey;
 
     /**
-     * 支付完成 - 回调地址
-     */
-    @ApiModelProperty(notes = "支付完成 - 回调地址", dataType = "String")
-    private String payedNotifyUrl;
-
-    /**
-     * 退款 - 回调地址
-     */
-    @ApiModelProperty(notes = "退款 - 回调地址", dataType = "String")
-    private String refundNotifyUrl;
-
-    /**
-     * 取消支付 - 回调地址
-     */
-    @ApiModelProperty(notes = "取消支付 - 回调地址", dataType = "String")
-    private String cancelNotifyUrl;
-
-    /**
      * 支付平台
      */
     @ApiModelProperty(notes = "支付平台", dataType = "PayPlatform")
