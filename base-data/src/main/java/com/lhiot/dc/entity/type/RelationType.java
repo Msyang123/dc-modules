ab8d615e40450fabac1be1e034442dac8b5bdee3
xiaojian <307041936@qq.com>
2018-12-11 10:18:02 +0800

    Merge branch '1.0' of http://172.16.10.198/lhiot/dc-modules into 1.0

--- base-data/src/main/java/com/lhiot/dc/entity/type/RelationType.java
+++ base-data/src/main/java/com/lhiot/dc/entity/type/RelationType.java
@@ -9,6 +9,10 @@ import lombok.Getter;
  */
 public enum RelationType {
     PRODUCT_DETAILS("商品详情"),
+    PRODUCT_SECTION("商品版块"),
+    CUSTOM_PLAN("定制计划"),
+    CUSTOM_PLAN_SECTION("定制版块"),
+    ARTICLE_DETAILS("文章详情"),
     STORE_LIVE_TELECAST("门店直播"),
     MORE_AMUSEMENT("多娱"),
     EXTERNAL_LINKS("外部链接");
